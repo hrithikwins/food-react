9c1d507774f2b70d980a97ff580e753403d39504
HRITHIK <hrithikrtiwari@gmail.com>
2020-10-15 18:39:50 +0530

    ..

--- src/views/main-page.js
+++ src/views/main-page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { HeaderComponent } from "./MainPage/header";
 
+
+         
+
 export function MainPage(){
     return(
         <>
-        <HeaderComponent/>
         </>
     );
 }
